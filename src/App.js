d20b5fc1677da7d9bad5358a744c7e71162f2fce
Giovanni Delle Lenti <giovanni.dellelenti@gmail.com>
2025-05-11 16:30:23 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
+
+import Login from './views/auth/Login';
 import NavBar from "./components/navbar/BlogNavbar";
 import Footer from "./components/footer/Footer";
 import Home from "./views/home/Home";
 import Blog from "./views/blog/Blog";
 import NewBlogPost from "./views/new/New";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './views/auth/Login'
+import ProtectedRoute from './components/auth/ProtectedRoute';
+
 
 function App() {
+
 
 
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/new" element={<NewBlogPost />} />
-        <Route path="/login" element={<Login />} />
-
+        <Route path="/" exact element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/blog/:id" element={<ProtectedRoute><Blog /></ProtectedRoute>} />
+        <Route path="/new" element={<ProtectedRoute><NewBlogPost /></ProtectedRoute>} />
+        <Route path="/login" element={<ProtectedRoute><Login /></ProtectedRoute>} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { fetchLoggedUser } from '../../utils/api';
+
+const ProtectedRoute = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const location = useLocation();
+
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await fetchLoggedUser();
+        setIsAuthenticated(true);
+
+      } catch (err) {
+        console.warn('Non autenticato:', err.message);
+        setIsAuthenticated(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, [location.pathname]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isAuthenticated && location.pathname === '/login') {
+    return <Navigate to="/" replace />;
+  } 
+  
+  return children;
+};
+
+export default ProtectedRoute;
